30423bb418facfbd01e01eaaed78e5ae5f8b53f9
betoko <christianbetoko@gmail.com>
2026-03-27 06:15:23 +0100

    modif footer

--- resources/views/livewire/components/footer.blade.php
+++ resources/views/livewire/components/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="row gx-5">
                 <div class="col-lg-4 col-md-6 footer-about">
                     <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-4">
-                        <a href="index.html" class="navbar-brand">
+                        <a href="{{ route('home') }}" class="navbar-brand">
                             {{-- <img src="{{ url('storage/' . $entreprise->logo_sans_fond) }}" alt="{{ $entreprise->name ?? 'Logo' }}" style="width: 100px; height: 100px;"> --}}
                             {{-- <span class="h1 text-white ms-2">{{ $entreprise->name ?? 'Ma Bannière' }}</span> --}}
                        <h1 class="m-0 text-white"></i>{{ $entreprise->name ?? 'ETS YOBA' }}</h1>

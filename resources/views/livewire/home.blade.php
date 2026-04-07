f11347922eb439d9a888261bba448def2bad729b
betoko <christianbetoko@gmail.com>
2026-04-07 14:24:10 +0100

    update home home

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -93,7 +93,59 @@
         </div>
     </div> 
     <!-- Facts Start -->
-
+  <!-- Features Start -->
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+                <h5 class="fw-bold text-primary text-uppercase">ETABLISSEMENT YOBA</h5>
+                <h1 class="mb-0">Votre Partenaire de Confiance</h1>
+            </div>
+            <div class="row g-5">
+                <div class="col-lg-4">
+                    <div class="row g-5">
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.2s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa- text-white"></i>
+                            </div>
+                            <h4>Agro pastoral</h4>
+                            <p class="mb-0"></p>
+                        </div>
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.6s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa-hospital text-white"></i>
+                            </div>
+                            <h4>CMC-B</h4>
+                            <p class="mb-0">Centre Médical Christ-ma-Bannière</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4  wow zoomIn" data-wow-delay="0.9s" style="min-height: 350px;">
+                    <div class="position-relative h-100">
+                        <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.1s" src="{{asset('assets/img/logo.png')}}" style="object-fit: contain;">
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="row g-5">
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.4s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa-camera text-white"></i>
+                            </div>
+                            <h4>Médias</h4>
+                            <p class="mb-0">Rédaction et Communication</p>
+                        </div>
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.8s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa-bus text-white"></i>
+                            </div>
+                            <h4>Transport</h4>
+                            <p class="mb-0"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Features Start -->
        <!-- Testimonial Start -->
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
